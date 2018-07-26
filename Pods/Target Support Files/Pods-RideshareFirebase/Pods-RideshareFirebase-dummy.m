ceb971724225dd51248bf6a129d546ff1a17b9d7
Sean Lin <seanlin1016@gmail.com>
2018-07-26 13:53:52 +0800

    Project Initialization

---
+++ Pods/Target Support Files/Pods-RideshareFirebase/Pods-RideshareFirebase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RideshareFirebase : NSObject
+@end
+@implementation PodsDummy_Pods_RideshareFirebase
+@end
---
+++ RideshareFirebase/HomeController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RideshareFirebase
+//
+//  Created by SEAN on 2018/7/26.
+//  Copyright © 2018 SEAN. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+
+
+}
+
